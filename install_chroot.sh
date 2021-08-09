13a2314f245f821b272763a07cbd6407415036bb
James Carr <jamescarr1993@gmail.com>
2021-08-09 19:20:21 +0100

    set x in scripts for debugging

--- install_chroot.sh
+++ install_chroot.sh
@@ -3,7 +3,7 @@
 # e - script stops on error
 # u - error if undefined variable
 # o pipefail - script fails if command piped fails
-set -euo pipefail
+set -euxo pipefail
 
 # load variables from file
 uefi=$(cat /var_uefi)
--- install_user.sh
+++ install_user.sh
@@ -3,7 +3,7 @@
 # e - script stops on error
 # u - error if undefined variable
 # o pipefail - script fails if command piped fails
-set -euo pipefail
+set -euxo pipefail
 
 mkdir -p "/home/$(whoami)/Documents"
 mkdir -p "/home/$(whoami)/Downloads"
